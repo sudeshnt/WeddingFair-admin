a8d1023d7812837a3b1a65256be3566cc23a4f79
sudeshnt <sudeshnt93@gmail.com>
2018-08-13 22:14:14 +0530

    added login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+
+import { DefaultLayoutComponent } from './containers';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
+const APP_CONTAINERS = [
+  DefaultLayoutComponent
+];
+
+import {
+  AppAsideModule,
+  AppBreadcrumbModule,
+  AppHeaderModule,
+  AppFooterModule,
+  AppSidebarModule,
+} from '@coreui/angular'
+
+// Import routing module
+import { AppRoutingModule } from './app.routing';
+
+// Import 3rd party components
+import { TabsModule } from 'ngx-bootstrap/tabs';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ...APP_CONTAINERS,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AppAsideModule,
+    AppBreadcrumbModule.forRoot(),
+    AppFooterModule,
+    AppHeaderModule,
+    AppSidebarModule,
+    PerfectScrollbarModule,
+    TabsModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
